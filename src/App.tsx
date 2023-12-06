bdfdd3dac7e3f196d774d51843656f2e258458cc
Varghese Babu <vargheseakhil1512@gmail.com>
2023-12-06 13:43:18 +0100

    chore: removes lock file and rename folder to make clone work

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg'
 import './App.css'
 import { BrowserRouter as Router, Switch , Route } from 'react-router-dom'
 import { Buyflow } from './buyflow'
-import { LandingPage } from './landing'
+import { LandingPage } from './welcome'
 
 const App = () => {
   return (
